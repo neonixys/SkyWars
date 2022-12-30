ba12d4c858f0a30ffcc835843a0b03af284fcbeb
alexeydoronin <neonixys@mail.ru>
2022-12-30 11:21:41 +0300

    upd

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, url_for, redirect
 
 from base import Arena
@@ -27,19 +26,22 @@ def menu_page():
 
 @app.route("/fight/")
 def start_fight():
-    # TODO выполняем функцию start_game экземпляра класса арена и передаем ему необходимые аргументы
-    # TODO рендерим экран боя (шаблон fight.html)
+    """
+    Выполняем функцию start_game экземпляра класса арена и передаем ему необходимые аргументы
+    Рендерим экран боя (шаблон fight.html)
+    """
     arena.start_game(player=heroes['player'], enemy=heroes['enemy'])
     return render_template('fight.html', heroes=heroes)
 
 
 @app.route("/fight/hit")
 def hit():
-    # TODO кнопка нанесения удара
-    # TODO обновляем экран боя (нанесение удара) (шаблон fight.html)
-    # TODO если игра идет - вызываем метод player.hit() экземпляра класса арены
-    # TODO если игра не идет - пропускаем срабатывание метода (простот рендерим шаблон с текущими данными)
-
+    """
+    Кнопка нанесения удара
+    Обновляем экран боя (нанесение удара) (шаблон fight.html)
+    Если игра идет - вызываем метод player.hit() экземпляра класса арены
+    Если игра не идет - пропускаем срабатывание метода (простот рендерим шаблон с текущими данными)
+    """
     if arena.game_is_running:
         result = arena.player_hit()
     else:
@@ -50,9 +52,10 @@ def hit():
 
 @app.route("/fight/use-skill")
 def use_skill():
-    # TODO кнопка использования скилла
-    # TODO логика пркатикчески идентична предыдущему эндпоинту
-
+    """
+    Кнопка использования скилла
+    Логика пркатикчески идентична предыдущему эндпоинту
+    """
     if arena.game_is_running:
         result = arena.player_use_skill()
     else:
@@ -63,10 +66,11 @@ def use_skill():
 
 @app.route("/fight/pass-turn")
 def pass_turn():
-    # TODO кнопка пропус хода
-    # TODO логика пркатикчески идентична предыдущему эндпоинту
-    # TODO однако вызываем здесь функцию следующий ход (arena.next_turn())
-
+    """
+    Кнопка пропус хода
+    Логика пркатикчески идентична предыдущему эндпоинту
+    Однако вызываем здесь функцию следующий ход (arena.next_turn())
+    """
     if arena.game_is_running:
         result = arena.next_turn()
     else:
@@ -77,16 +81,19 @@ def pass_turn():
 
 @app.route("/fight/end-fight")
 def end_fight():
-    # TODO кнопка завершить игру - переход в главное меню
+    """
+    Кнопка завершить игру - переход в главное меню
+    """
     return render_template("index.html", heroes=heroes)
 
 
 @app.route("/choose-hero/", methods=['post', 'get'])
 def choose_hero():
-    # TODO кнопка выбор героя. 2 метода GET и POST
-    # TODO на GET отрисовываем форму.
-    # TODO на POST отправляем форму и делаем редирект на эндпоинт choose enemy
-
+    """
+    Кнопка выбор героя. 2 метода GET и POST
+    На GET отрисовываем форму.
+    На POST отправляем форму и делаем редирект на эндпоинт choose enemy
+    """
     if request.method == 'GET':
         header = "Выберите игрока"
         equipment = Equipment()
@@ -100,7 +107,6 @@ def choose_hero():
         }
         return render_template('hero_choosing.html', result=result)
     if request.method == 'POST':
-
         name = request.form['name']
         weapon_name = request.form['weapon']
         armor_name = request.form['armor']
@@ -112,11 +118,14 @@ def choose_hero():
 
         return redirect(url_for('choose_enemy'))
 
+
 @app.route("/choose-enemy/", methods=['post', 'get'])
 def choose_enemy():
-    # TODO кнопка выбор соперников. 2 метода GET и POST
-    # TODO также на GET отрисовываем форму.
-    # TODO а на POST отправляем форму и делаем редирект на начало битвы
+    """
+    Кнопка выбор соперников. 2 метода GET и POST
+    На GET отрисовываем форму.
+    На POST отправляем форму и делаем редирект на начало битвы
+    """
     if request.method == 'GET':
         header = "Выберите противника"
         equipment = Equipment()
--- base.py
+++ base.py
@@ -19,21 +19,23 @@ class Arena(metaclass=BaseSingleton):
     battle_result = None
 
     def start_game(self, player: BaseUnit, enemy: BaseUnit):
-        # TODO НАЧАЛО ИГРЫ -> None
-        # TODO присваиваем экземпляру класса аттрибуты "игрок" и "противник"
-        # TODO а также выставляем True для свойства "началась ли игра"
-
+        """
+        НАЧАЛО ИГРЫ -> None
+        Присваиваем экземпляру класса аттрибуты "игрок" и "противник"
+        Выставляем True для свойства "началась ли игра
+        """
         self.player = player
         self.enemy = enemy
         self.game_is_running = True
 
     def _check_players_hp(self):
-        # TODO ПРОВЕРКА ЗДОРОВЬЯ ИГРОКА И ВРАГА
-        # TODO проверка здоровья игрока и врага и возвращение результата строкой:
-        # TODO может быть три результата:
-        # TODO Игрок проиграл битву, Игрок выиграл битву, Ничья и сохраняем его в аттрибуте (self.battle_result)
-        # TODO если Здоровья игроков в порядке то ничего не происходит
-
+        """
+        ПРОВЕРКА ЗДОРОВЬЯ ИГРОКА И ВРАГА
+        Проверка здоровья игрока и врага и возвращение результата строкой:
+        Может быть три результата:
+        Игрок проиграл битву, Игрок выиграл битву, Ничья и сохраняем его в аттрибуте (self.battle_result)
+        Если Здоровья игроков в порядке то ничего не происходит
+        """
         if self.player.hp > 0 and self.enemy.hp > 0:
             return None
 
@@ -47,10 +49,11 @@ class Arena(metaclass=BaseSingleton):
         return self._end_game()
 
     def _stamina_regeneration(self):
-        # TODO регенерация здоровья и стамины для игрока и врага за ход
-        # TODO в этом методе к количеству стамины игрока и врага прибавляется константное значение.
-        # TODO главное чтобы оно не привысило максимальные значения (используйте if)
-
+        """
+        Регенерация здоровья и стамины для игрока и врага за ход
+        В этом методе к количеству стамины игрока и врага прибавляется константное значение.
+        Главное чтобы оно не привысило максимальные значения (используйте if)
+        """
         units = (self.player, self.enemy)
 
         for unit in units:
@@ -60,14 +63,15 @@ class Arena(metaclass=BaseSingleton):
                 unit.stamina += self.STAMINA_PER_ROUND
 
     def next_turn(self):
-        # TODO СЛЕДУЮЩИЙ ХОД -> return result | return self.enemy.hit(self.player)
-        # TODO срабатывает когда игроп пропускает ход или когда игрок наносит удар.
-        # TODO создаем поле result и проверяем что вернется в результате функции self._check_players_hp
-        # TODO если result -> возвращаем его
-        # TODO если же результата пока нет и после завершения хода игра продолжается,
-        # TODO тогда запускаем процесс регенирации стамины и здоровья для игроков (self._stamina_regeneration)
-        # TODO и вызываем функцию self.enemy.hit(self.player) - ответный удар врага
-
+        """
+        СЛЕДУЮЩИЙ ХОД -> return result | return self.enemy.hit(self.player)
+        Срабатывает когда игроп пропускает ход или когда игрок наносит удар.
+        Создаем поле result и проверяем что вернется в результате функции self._check_players_hp
+        Если result -> возвращаем его
+        Если же результата пока нет и после завершения хода игра продолжается,
+        Тогда запускаем процесс регенирации стамины и здоровья для игроков (self._stamina_regeneration)
+        Вызываем функцию self.enemy.hit(self.player) - ответный удар врага
+        """
         result = self._check_players_hp()
         if result is not None:
             return result
@@ -76,30 +80,34 @@ class Arena(metaclass=BaseSingleton):
             return self.enemy.hit(self.player)
 
     def _end_game(self):
-        # TODO КНОПКА ЗАВЕРШЕНИЕ ИГРЫ - > return result: str
-        # TODO очищаем синглтон - self._instances = {}
-        # TODO останавливаем игру (game_is_running)
-        # TODO возвращаем результат
+        """
+        КНОПКА ЗАВЕРШЕНИЕ ИГРЫ - > return result: str
+        Очищаем синглтон - self._instances = {}
+        Останавливаем игру (game_is_running)
+        Возвращаем результат
+        """
         self.instances = {}
         self.game_is_running = False
         return self.battle_result
 
     def player_hit(self):
-        # TODO КНОПКА УДАР ИГРОКА -> return result: str
-        # TODO получаем результат от функции self.player.hit
-        # TODO запускаем следующий ход
-        # TODO возвращаем результат удара строкой
-
+        """
+        КНОПКА УДАР ИГРОКА -> return result: str
+        Получаем результат от функции self.player.hit
+        Запускаем следующий ход
+        Возвращаем результат удара строкой
+        """
         result = self.player.hit(self.enemy)
         turn_result = self.next_turn()
         return f'{result}\n{turn_result}'
 
     def player_use_skill(self):
-        # TODO КНОПКА ИГРОК ИСПОЛЬЗУЕТ УМЕНИЕ
-        # TODO получаем результат от функции self.use_skill
-        # TODO включаем следующий ход
-        # TODO возвращаем результат удара строкой
-
+        """
+        КНОПКА ИГРОК ИСПОЛЬЗУЕТ УМЕНИЕ
+        Получаем результат от функции self.use_skill
+        Включаем следующий ход
+        Возвращаем результат удара строкой
+        """
         result = self.player.use_skill(self.enemy)
         turn_result = self.next_turn()
         return f'{result}\n{turn_result}'
