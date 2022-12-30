e3b070f6552faf8170a610d2f95ddce0e66efc22
alexeydoronin <neonixys@mail.ru>
2022-12-30 11:05:37 +0300

    Initial commit

---
+++ equipment.py
@@ -0,0 +1,90 @@
+import json
+from dataclasses import dataclass
+from random import uniform
+from typing import Optional
+
+import marshmallow
+import marshmallow_dataclass
+
+
+@dataclass
+class Armor:
+    id: int
+    name: str
+    defence: float
+    stamina_per_turn: float
+
+
+@dataclass
+class Weapon:
+    id: int
+    name: str
+    min_damage: float
+    max_damage: float
+    stamina_per_hit: float
+
+    @property
+    def damage(self):
+        return round(uniform(self.min_damage, self.max_damage), 1)
+
+
+@dataclass
+class EquipmentData:
+    """
+    Класс содержит 2 списка - с оружием и с броней
+    """
+    weapons: list[Weapon]
+    armors: list[Armor]
+
+
+class Equipment:
+
+    def __init__(self):
+        self.equipment = self._get_equipment_data()
+
+    def get_weapon(self, weapon_name) -> Optional[Weapon]:
+        """
+        Метод возвращает объект оружия по имени
+        """
+        for weapon in self.equipment.weapons:
+            if weapon_name == weapon_name:
+                return weapon
+        return None
+
+    def get_armor(self, armor_name) -> Optional[Armor]:
+        """
+        Метод возвращает объект брони по имени
+        """
+        for armor in self.equipment.armors:
+            if armor.name == armor_name:
+                return armor
+        return None
+
+    def get_weapons_names(self) -> list:
+        """
+        :return: список с оружием
+        """
+        return [
+            weapon.name for weapon in self.equipment.weapons
+        ]
+
+    def get_armors_names(self) -> list:
+        """
+        :return: список с броней
+        """
+        return [
+            armor.name for armor in self.equipment.armors
+        ]
+
+    @staticmethod
+    def _get_equipment_data() -> EquipmentData:
+        """
+        Метод загружает json в переменную EquipmentData
+        """
+        with open("./data/equipment.json") as file:
+            data = json.load(file)
+            equipment_schema = marshmallow_dataclass.class_schema(EquipmentData)
+            try:
+                return equipment_schema().load(data)
+            except marshmallow.exceptions.ValidationError:
+                raise ValueError
