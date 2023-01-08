998af74400dfacdf925e9ed823b04f8c4387d03b
alexeydoronin <neonixys@mail.ru>
2023-01-08 14:51:01 +0500

    alex85doronin

--- equipment.py
+++ equipment.py
@@ -82,7 +82,7 @@ class Equipment:
         """
         Метод загружает json в переменную EquipmentData
         """
-        my_absolute_path = './data/equipment.json'
+        my_absolute_path = "./data/equipment.json"
         with open(my_absolute_path, 'r', encoding='utf-8') as file:
 
             data = json.load(file)
